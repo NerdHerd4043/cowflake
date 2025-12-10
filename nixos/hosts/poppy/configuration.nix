cd1e2487384d4166dec5030d99134a99ad3f4569
NullCube <nullcub3@gmail.com>
2025-12-09 21:37:14 -0800

    poppy: use latest kernel for better USB wifi adapter

--- nixos/hosts/poppy/configuration.nix
+++ nixos/hosts/poppy/configuration.nix
@@ -119,6 +119,9 @@
     "9.9.9.9"
   ];
 
+  # Use latest kernel to get driver for better USB wifi adapter
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "25.05"; # Did you read the comment?
