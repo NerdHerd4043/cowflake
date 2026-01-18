e72f907567e9bdd2155fe8742b1e96faf31579ff
NullCube <nullcub3@gmail.com>
2026-01-18 03:45:03 -0800

    laptop-24: add graphics config

--- nixos/hosts/laptop-24/configuration.nix
+++ nixos/hosts/laptop-24/configuration.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 {
   imports = [
     ./disk-configuration.nix
@@ -10,6 +10,20 @@
   };
 
   networking.hostName = "laptop-24";
+
+  # For i3-4005U:
+  hardware.intel-gpu-tools.enable = true;
+  hardware.graphics = {
+    enable = true;
+    extraPackages = with pkgs; [
+      intel-vaapi-driver
+      intel-ocl
+    ];
+  };
+  environment.sessionVariables = {
+    LIBVA_DRIVER_NAME = "i965";
+    VDPAU_DRIVER = "va_gl";
+  };
 
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
