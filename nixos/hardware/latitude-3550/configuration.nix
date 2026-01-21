ad7f5cf38c29f60405eb98b985fb79b89d4677d4
NullCube <nullcub3@gmail.com>
2026-01-21 06:40:39 -0800

    flake: set system.stateVersion for laptop-08

--- nixos/hardware/latitude-3550/configuration.nix
+++ nixos/hardware/latitude-3550/configuration.nix
@@ -55,8 +55,4 @@
     LIBVA_DRIVER_NAME = "i965";
     VDPAU_DRIVER = "va_gl";
   };
-
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "25.05"; # Did you read the comment?
 }
